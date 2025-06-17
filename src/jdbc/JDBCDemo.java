53f7adf465c7aee3d4ed51daff1aacb41369422d
ManasiAmarRugle <manasirugle0211@gmail.com>
2025-06-17 18:19:41 +0530

    updatated the code

--- src/jdbc/JDBCDemo.java
+++ src/jdbc/JDBCDemo.java
@@ -4,28 +4,64 @@ import java.sql.*;
 
 public class JDBCDemo {
     public static void main(String[] args) {
-
-        String url = "jdbc:postgresql://localhost:5432/studentdb";
-        String user = "postgres";
-        String password = "root";
-
         try {
+            Connection connection = getDatabaseConnection();
+            System.out.println("Connection to database successful - " + connection);
 
-            Connection conn = DriverManager.getConnection(url, user, password);
-            System.out.println("Connected successfully!");
+            insertStudent(connection);
 
+            fetchStudent(connection);
 
+            updateStudent(connection);
+
+            fetchStudent(connection);
+
+            deleteStudent(connection);
+
+            fetchStudent(connection);
+
+            closeConnection(connection);
+
+        } catch (Exception e) {
+            System.out.println("Exception occurred while calling functions - " + e.getMessage() + e);
+        }
+    }
+
+    public static Connection getDatabaseConnection() {
+        try {
+            String url = "jdbc:postgresql://localhost:5432/studentdb";
+            String user = "postgres";
+            String password = "root";
+            Connection connection = DriverManager.getConnection(url, user, password);
+            System.out.println("Connection to database established successfully !");
+            return connection;
+        } catch (SQLException e) {
+            System.out.println("Exception occurred while connecting to database - " + e.getMessage() + e);
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static void insertStudent(Connection connection) {
+        try {
             String insertSQL = "INSERT INTO students (name, marks) VALUES (?, ?)";
-            PreparedStatement insertStmt = conn.prepareStatement(insertSQL);
-            insertStmt.setString(1, "Aditya");
-            insertStmt.setInt(2, 99 );
+            PreparedStatement insertStmt = connection.prepareStatement(insertSQL);
+            insertStmt.setString(1, "Test177");
+            insertStmt.setInt(2, 99);
             insertStmt.executeUpdate();
             System.out.println("Data inserted.");
+            insertStmt.close();
+        } catch (SQLException e) {
+            System.out.println("Exception occurred while inserting student - " + e.getMessage() + e);
+            throw new RuntimeException(e);
+        }
+    }
 
-
-            String selectSQL = "SELECT * FROM students";
-            Statement selectStmt = conn.createStatement();
-            ResultSet rs = selectStmt.executeQuery(selectSQL);
+    public static void fetchStudent(Connection connection) {
+        try {
+            String selectSQL = "SELECT * FROM students WHERE name = ?";
+            PreparedStatement selectStmt = connection.prepareStatement(selectSQL);
+            selectStmt.setString(1, "Test177");
+            ResultSet rs = selectStmt.executeQuery();
 
             System.out.println("Fetching data:");
             while (rs.next()) {
@@ -35,13 +71,52 @@ public class JDBCDemo {
                 System.out.println("ID: " + id + ", Name: " + name + ", Marks: " + marks);
             }
 
-
-            rs.close();
-            insertStmt.close();
             selectStmt.close();
-            conn.close();
+            rs.close();
         } catch (SQLException e) {
-            e.printStackTrace();
+            System.out.println("Exception occurred while fetching student - " + e.getMessage() + e);
+            throw new RuntimeException(e);
         }
     }
+
+    public static void updateStudent(Connection connection) {
+        try {
+            String updateSQL = "UPDATE students SET marks = ? WHERE name = ?";
+            PreparedStatement updateStmt = connection.prepareStatement(updateSQL);
+            updateStmt.setInt(1, 77);
+            updateStmt.setString(2, "Test177");
+            updateStmt.executeUpdate();
+            System.out.println("Data updated.");
+            updateStmt.close();
+
+        } catch (SQLException e) {
+            System.out.println("Exception occurred while updating student - " + e.getMessage() + e);
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static void deleteStudent(Connection connection) {
+        try {
+            String deleteSQL = "DELETE FROM students WHERE name = ?";
+            PreparedStatement deleteStmt = connection.prepareStatement(deleteSQL);
+            deleteStmt.setString(1, "Test177");
+            deleteStmt.executeUpdate();
+            System.out.println("Data deleted.");
+            deleteStmt.close();
+        } catch (Exception e) {
+            System.out.println("Exception occurred while deleting student - " + e.getMessage() + e);
+            throw new RuntimeException(e);
+        }
+    }
+
+
+    public static void closeConnection(Connection connection) {
+        try {
+            connection.close();
+        } catch (SQLException e) {
+            System.out.println("Exception occurred while closing database connection - " + e.getMessage() + e);
+            throw new RuntimeException(e);
+        }
+    }
+
 }
